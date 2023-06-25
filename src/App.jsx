22e95647af3e3e9ed568f89f506dd804342e5982
VectoRaxo <julialberti666@gmail.com>
2023-06-25 17:30:22 +0200

    Portfolio starting

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useState } from 'react';
+
+function App() {
+  
+    
+    const [animation, setAnimation] = useState(false);
+    
+  
+    const handleClic = () => {
+      // Cambiar el estado de "animation" al hacer clic
+      
+      setAnimation(!animation);
+     
+    };
+    
+   
+  
+
+
+
+
+  return (
+    <>
+      <Navbar fixed='top' expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Julián Alberti</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+
+    <div id="intro-button" className={`border-button ${animation ? 'introAnimation' : ''}`}
+        onClick={handleClic}>
+    <Container className='name-button'>
+      <h1>Julián Alberti</h1> 
+      <sub>Web Developer</sub>
+      </Container>
+      
+      </div>
+    </>
+  )
+}
+
+export default App
