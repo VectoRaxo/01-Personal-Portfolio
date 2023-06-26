09e0e70a9ea4b56b8b3ba3e582d58086ab5d55eb
VectoRaxo <julialberti666@gmail.com>
2023-06-26 11:06:36 +0200

    intro update

--- src/App.jsx
+++ src/App.jsx
@@ -29,11 +29,11 @@ function App() {
     <>
       <Navbar fixed='top' expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Julián Alberti</Navbar.Brand>
+        <Navbar.Brand href="http://localhost:5173/">Julián Alberti</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="http://localhost:5173/">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
