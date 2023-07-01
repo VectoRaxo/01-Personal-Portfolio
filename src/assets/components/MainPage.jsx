96a517e64dc84ab4d5ae35beba1317587ca3f500
VectoRaxo <julialberti666@gmail.com>
2023-07-01 17:19:30 +0200

    adding tabs

--- src/assets/components/MainPage.jsx
+++ src/assets/components/MainPage.jsx
@@ -6,11 +6,15 @@ import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import { ShowModal } from "./Modal";
 import { NavBar } from './NavBar';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
 export const MainPage = () => {
     const [modalShow, setModalShow] = React.useState(false);
     const [selectedImage, setSelectedImage] = React.useState(null);
     const [isMobile, setIsMobile] = useState(false);
+    const [key, setKey] = useState('home');
+
     const handleImageClick = (src, alt) => {
       setSelectedImage({ src, alt });
       setModalShow(true);
@@ -27,77 +31,173 @@ export const MainPage = () => {
         window.removeEventListener('resize', handleResize);
       };
     }, [])
+
+
   return (
 <>
-    <NavBar />
+    
     <Container className='main-frame introMainPage'>
-    <Row>
-        <div className='main-title'>
-        <h1>Julián Alberti</h1>
-        </div>
-    </Row>
-    <Row className="formButton">
-        <div data-bs-toggle="modal" data-bs-target="#contactModal" onClick={() => handleImageClick('contact')}>
-        <a className="btn btn-xl btn-outline-light" id='contactModal'>
-            Contact me!
-        </a>
-        </div>
-    </Row>
-
-    <Row>
-        <h1 className='sub-main-title'>About Me</h1>
-    </Row>
-    <Row className='about-me'>
-        <h5>
-        I am an enthusiastic person, creative, and always willing to explore new ideas and concepts. <br />
-        I have a desire to learn, and that drives me to develop my skills and apply them in innovative projects.
-        </h5>
-    </Row>
-    <Row className='thumbnails'>
-        <div>
-        {isMobile ? (
-            <Carousel>
-            <Carousel.Item data-bs-toggle="modal" data-bs-target="#martaModal"
-                    onClick={() => handleImageClick("src/assets/img/vector/640/marta640.PNG", "Netherworld Queen")}  >
-                <Image id="martaModal" src="src/assets/img/vector/marta1.PNG" alt='First' rounded className="img-fluid"  />
-            </Carousel.Item>
-            <Carousel.Item data-bs-toggle="modal" data-bs-target="#abModal"
-                    onClick={() => handleImageClick("src/assets/img/vector/640/ab640.PNG", "Woman with flower")}  >
-                <Image id="abModal" src="src/assets/img/vector/ab.PNG" alt='Second' rounded className="img-fluid"  />
-            </Carousel.Item>
-            <Carousel.Item data-bs-toggle="modal" data-bs-target="#noobModal"
-                    onClick={() => handleImageClick("src/assets/img/vector/640/noob640.png", "Noob-Saibot NEON")}  >
-                <Image id="noobModal" src="src/assets/img/vector/noob.png" alt='Noob-Saibot NEON' rounded className="img-fluid"  />
-            </Carousel.Item>
-            </Carousel>
-        ) : (
-            <Container>
-            <Row>
-                <Col xs={6} md={4} className="mx-1">
-                <div className="image-wrapper" data-bs-toggle="modal" data-bs-target="#martaModal"
-                    onClick={() => handleImageClick("src/assets/img/vector/640/marta640.PNG", "Netherworld Queen")}>
-                    <Image id="martaModal" src="src/assets/img/vector/marta1.PNG" alt='First' rounded className="img-fluid" />
-                </div>
-                </Col>
-                <Col xs={6} md={4} className="mx-1">
-                <div className="image-wrapper" data-bs-toggle="modal" data-bs-target="#abModal"
-                    onClick={() => handleImageClick("src/assets/img/vector/640/ab640.PNG", "Woman with flower")}>
-                    <Image id="abModal" src="src/assets/img/vector/ab.PNG" alt='Second' rounded className="img-fluid"/>
-                </div>
-                </Col>
-                <Col xs={6} md={4} className="mx-1">
-                <div className="image-wrapper" data-bs-toggle="modal" data-bs-target="#noobModal"
-                    onClick={() => handleImageClick("src/assets/img/vector/640/noob640.png", "Noob-Saibot NEON")}>
-                    <Image id="noobModal" src="src/assets/img/vector/noob.png" alt='Noob-Saibot NEON' rounded className="img-fluid"/>
-                </div>
-                </Col>
-            </Row>
-            </Container>
+        <NavBar />
+            <Col className='contentFrame'>
+                <Row>
+                    <div className='main-title'>
+                    <h1>Julián Alberti</h1>
+                    </div>
+                </Row>
+                <Row>
+                    <h1 className='sub-main-title'>About Me</h1>
+                </Row>
+                <Row className='about-me'>
+                    <h5>
+                    I am an enthusiastic person, creative, and always willing to explore new ideas and concepts. <br />
+                    I have a desire to learn, and that drives me to develop my skills and apply them in innovative projects.
+                    </h5>
+                </Row>
             
-        )}
+        <Container fluid="xs">
+                <Tabs
+                id="justify-tab-example"
+                activeKey={key}
+                onSelect={(k) => setKey(k)}
+                className="mb-3"
+                justify
+                >
+                <Tab eventKey="home" title="My Profile" style={{ textAlign: 'left', color: 'white', width: '90%', margin:'auto' }}>
+                    <Row>
+                        <h1>Experience</h1>
+                    </Row>
+                    <Row style={{width: '90%', margin:'auto' }}>
+                        <h5>
+                            Web developer - Freelance (2023)
+                        </h5>
+                    </Row>
+                    <Row style={{width: '90%', marginLeft:'4em', fontStyle:'italic'}}>
+                        <h6>
+                        · Development and maintenance of websites. <br />
+                        · Use of Javascript, Typescript, React.js
+                        </h6>
+                    </Row>
+                    <Row style={{width: '90%', margin:'auto' }}>
+                        <h5>
+                            Developing of videogames in Unity (2023)
+                        </h5>
+                    </Row>
+                    <Row style={{width: '90%', marginLeft:'4em', fontStyle:'italic'}}>
+                        <h6>
+                        · Development in C#, implementation of interfaces, <br/> use of
+                        GitHub and creation of assets.
 
-        </div>
-    </Row>
+                        </h6>
+                    </Row>
+                    <Row style={{width: '90%', margin:'auto' }}>
+                        <h5>
+                        Technical support and customer service (2021-2022)
+                        </h5>
+                    </Row>
+                    <Row style={{width: '90%', marginLeft:'4em', fontStyle:'italic'}}>
+                        <h6>
+                        · Incident management and database management. <br/>
+                        · Web development and graphic design. <br/>
+                        · Teamwork.
+                        </h6>
+                    </Row>
+                    <Row style={{width: '90%', margin:'auto' }}>
+                        <h5>
+                        Manager - El Baúl Gráfico (2015-2019)
+                        </h5>
+                    </Row>
+                    <Row style={{width: '90%', marginLeft:'4em', fontStyle:'italic'}}>
+                        <h6>
+                        · Graphic and textile printing, logo design, signage, and
+                            event services. <br/>
+                        · Proficient in Adobe Suite, web design, and printing
+                            machinery.
+                        </h6>
+                    </Row>
+                    <Row style={{width: '90%', margin:'auto' }}>
+                        <h5>
+                        Application Developer (Internship)
+                        </h5>
+                    </Row>
+                    <Row style={{width: '90%', marginLeft:'4em', fontStyle:'italic'}}>
+                        <h6>
+                        · Generation of forms, reports, and Excel handling <br/>
+                        · Development of applications with Visual Basic and .NET.
+                        </h6>
+                    </Row>
+                    <Row>
+                        <h1>Education</h1>
+                    </Row>
+                    <Row style={{width: '90%', marginLeft:'4em', fontStyle:'italic'}}>
+                        <h6>
+                        · Generation of forms, reports, and Excel handling <br/>
+                        · Development of applications with Visual Basic and .NET.
+                        </h6>
+                    </Row>
+                    
+                    
+                </Tab>
+
+
+                <Tab eventKey="profile" title="My Hobbies">
+                <Row className='thumbnails'>
+                    <div>
+                    {isMobile ? (
+                        <Carousel>
+                        <Carousel.Item data-bs-toggle="modal" data-bs-target="#martaModal"
+                                onClick={() => handleImageClick("src/assets/img/vector/640/marta640.PNG", "Netherworld Queen")}  >
+                            <Image id="martaModal" src="src/assets/img/vector/marta1.PNG" alt='First' rounded className="img-fluid"  />
+                        </Carousel.Item>
+                        <Carousel.Item data-bs-toggle="modal" data-bs-target="#abModal"
+                                onClick={() => handleImageClick("src/assets/img/vector/640/ab640.PNG", "Woman with flower")}  >
+                            <Image id="abModal" src="src/assets/img/vector/ab.PNG" alt='Second' rounded className="img-fluid"  />
+                        </Carousel.Item>
+                        <Carousel.Item data-bs-toggle="modal" data-bs-target="#noobModal"
+                                onClick={() => handleImageClick("src/assets/img/vector/640/noob640.png", "Noob-Saibot NEON")}  >
+                            <Image id="noobModal" src="src/assets/img/vector/noob.png" alt='Noob-Saibot NEON' rounded className="img-fluid"  />
+                        </Carousel.Item>
+                        </Carousel>
+                    ) : (
+                        <Container>
+                        <Row>
+                            <Col xs={6} md={4} className="mx-1">
+                            <div className="image-wrapper" data-bs-toggle="modal" data-bs-target="#martaModal"
+                                onClick={() => handleImageClick("src/assets/img/vector/640/marta640.PNG", "Netherworld Queen")}>
+                                <Image id="martaModal" src="src/assets/img/vector/marta1.PNG" alt='First' rounded className="img-fluid" />
+                            </div>
+                            </Col>
+                            <Col xs={6} md={4} className="mx-1">
+                            <div className="image-wrapper" data-bs-toggle="modal" data-bs-target="#abModal"
+                                onClick={() => handleImageClick("src/assets/img/vector/640/ab640.PNG", "Woman with flower")}>
+                                <Image id="abModal" src="src/assets/img/vector/ab.PNG" alt='Second' rounded className="img-fluid"/>
+                            </div>
+                            </Col>
+                            <Col xs={6} md={4} className="mx-1">
+                            <div className="image-wrapper" data-bs-toggle="modal" data-bs-target="#noobModal"
+                                onClick={() => handleImageClick("src/assets/img/vector/640/noob640.png", "Noob-Saibot NEON")}>
+                                <Image id="noobModal" src="src/assets/img/vector/noob.png" alt='Noob-Saibot NEON' rounded className="img-fluid"/>
+                            </div>
+                            </Col>
+                        </Row>
+                        </Container>
+                        
+                    )}
+
+                    </div>
+        </Row>
+                </Tab>
+                <Tab eventKey="contact" title="Contact">
+                    <Row className="formButton">
+                        <div data-bs-toggle="modal" data-bs-target="#contactModal" onClick={() => handleImageClick('contact')}>
+                        <a className="btn btn-xl btn-outline-light" id='contactModal'>
+                            Contact me!
+                        </a>
+                        </div>
+                    </Row>
+                </Tab>
+                </Tabs> 
+        </Container>
+    </Col>
     <Row>
         <ShowModal
         show={modalShow}
